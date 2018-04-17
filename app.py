32afb551abc094e2b1bf556dca7dd01a91b26225
Łukasz Langa <lukasz@langa.pl>
2018-04-17 11:40:25 -0700

    Replace example source with a more thorough one

--- app.py
+++ app.py
@@ -5,35 +5,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-SOURCE_CODE = textwrap.dedent(
-    """
-    x = {  'a':37,'b':42,
-
-    'c':927}
-
-    y = 'hello ''world'
-    z = 'hello '+'world'
-    a = 'hello {}'.format('world')
-    class foo  (     object  ):
-      def f    (self   ):
-        return       37*-2
-      def g(self, x,y=42):
-          return y
-    def f  (   a ) :
-      return      37-a[42-x :  y**3]
-
-    """
-)
-
-
 @app.route("/", methods=["POST", "GET"])
 def index():
-    source = SOURCE_CODE
-    line_length = 88
-
     if request.method == "POST":
         source = request.form["source"]
         line_length = int(request.form["line_length"])
+    else:
+        source = render_template("source.py")
+        line_length = 60
 
     try:
         formatted = black.format_str(source, line_length=line_length)
