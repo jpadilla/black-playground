64b750bc49f9714a6044e2931bc0699594b1f1ae
Lukas Geiger <lukas.geiger94@gmail.com>
2019-03-04 23:59:06 +0000

    Reduce header size

--- web/components/header.js
+++ web/components/header.js
@@ -3,18 +3,22 @@ import Link from 'next/link';
 import Icon from './icon';
 
 const Head = (props) => (
-  <div className="flex justify-between content-center items-center p-4">
+  <div className="flex justify-between content-center items-center pl-4 pr-4">
     <div className="flex flex-col">
-      <h1 className="text-left my-2">
-        Black <span className="text-sm text-grey-dark">{props.version}</span>
-      </h1>
       <Link href="http://black.readthedocs.io/en/latest/">
-        <a className="font-semibold text-xs text-grey-darkest hover:text-black">
-          The uncompromising Python code formatter.
+        <a
+          style={{ textDecoration: 'inherit', color: 'inherit' }}
+          className="hover:text-black">
+          <h1 className="text-left my-2">
+            Black{' '}
+            <span className="text-sm text-grey-dark">
+              {props.version} - The uncompromising Python code formatter.
+            </span>
+          </h1>
         </a>
       </Link>
     </div>
-    <div className="flex justify-between content-center items-center">
+    <div className="flex justify-between content-center items-baseline mt-2">
       <p className="text-xs text-grey-darkest font-semibold mr-2">
         Playground built by{' '}
         <Link href="https://jpadilla.com">
